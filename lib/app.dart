4d3ed01f8826d801f3981e43da8caf23d1c317d6
codeZeus-Ojonaw <lewechigodsfavour@gmail.com>
2025-01-06 01:08:13 +0100

    ref: Refactor main.dart

---
+++ lib/app.dart
@@ -0,0 +1,63 @@
+
+import 'package:defifundr_mobile/bloc_provider.dart';
+import 'package:defifundr_mobile/core/routers/_routes.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class MyApp extends ConsumerStatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  ConsumerState<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends ConsumerState<MyApp> {
+  @override
+  void initState() {
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarIconBrightness: Brightness.light,
+        statusBarBrightness: Brightness.light,
+        systemNavigationBarIconBrightness: Brightness.light,
+      ),
+    );
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(410, 890),
+      minTextAdapt: true,
+      splitScreenMode: false,
+      child: MultiBlocProvider(
+        providers: appProviders,
+        child: MaterialApp.router(
+          debugShowCheckedModeBanner: false,
+          title: 'DeFiFundr',
+          scrollBehavior: const _AppScrollBehavior(),
+          theme: ThemeData(
+            primarySwatch: Colors.grey,
+          ),
+          routeInformationProvider: AppRouter.router.routeInformationProvider,
+          routeInformationParser: AppRouter.router.routeInformationParser,
+          routerDelegate: AppRouter.router.routerDelegate,
+        ),
+      ),
+    );
+  }
+
+}
+
+class _AppScrollBehavior extends ScrollBehavior {
+  const _AppScrollBehavior();
+
+  @override
+  ScrollPhysics getScrollPhysics(BuildContext context) {
+    return const BouncingScrollPhysics();
+  }
+}
