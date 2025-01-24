91ea91826b790c4f5aaeabd8d88f6a8ed8d76939
Anh Hoang Nguyen <hoangkianh31592@gmail.com>
2025-01-24 18:33:04 +0700

    feat: verify opt screen

--- lib/features/authentication/presentation/signup/view/verify_otp_screen.dart
+++ lib/features/authentication/presentation/signup/view/verify_otp_screen.dart
@@ -1,16 +1,17 @@
+import 'package:defifundr_mobile/core/global/constants/app_icons.dart';
+import 'package:defifundr_mobile/core/global/constants/app_images.dart';
 import 'package:defifundr_mobile/core/global/constants/size.dart';
 import 'package:defifundr_mobile/core/global/themes/color_scheme.dart';
 import 'package:defifundr_mobile/core/routers/routes_constants.dart';
-import 'package:defifundr_mobile/core/shared/button/buttons.dart';
 import 'package:defifundr_mobile/core/shared/custom_tooast/custom_tooast.dart';
 import 'package:defifundr_mobile/core/utils/loading_overlay.dart';
 import 'package:defifundr_mobile/features/authentication/presentation/signup/states/bloc/sign_up_bloc.dart';
 import 'package:defifundr_mobile/features/authentication/presentation/signup/states/verify_bloc/bloc/verify_otp_bloc.dart';
-import 'package:defifundr_mobile/features/authentication/presentation/signup/widgets/resend_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:pinput/pinput.dart';
@@ -22,17 +23,13 @@ import '../../../../../core/global/constants/app_texts.dart';
 class VerifyOTPScreen extends ConsumerStatefulWidget {
   final String username;
   final String email;
-  const VerifyOTPScreen(
-      {required this.username, required this.email, super.key});
+  const VerifyOTPScreen({required this.username, required this.email, super.key});
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() =>
-      _VerifyOTPScreenState();
+  ConsumerState<ConsumerStatefulWidget> createState() => _VerifyOTPScreenState();
 }
 
-class _VerifyOTPScreenState extends ConsumerState<VerifyOTPScreen>
-    with LoadingOverlayMixin {
-  final ValueNotifier<bool> isEnabled = ValueNotifier<bool>(false);
+class _VerifyOTPScreenState extends ConsumerState<VerifyOTPScreen> with LoadingOverlayMixin {
   final ValueNotifier<String> otp = ValueNotifier<String>("");
   final TextEditingController _otpController = TextEditingController();
   late final CountdownController _controller;
@@ -61,12 +58,28 @@ class _VerifyOTPScreenState extends ConsumerState<VerifyOTPScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: PreferredSize(
-      //   preferredSize: Size(context.screenWidth(), 60),
-      //   child: const DeFiRaiseAppBar(
-      //     title: 'Notifications',
-      //   ),
-      // ),
+      appBar: AppBar(
+        title: Text(
+          AppTexts.signUp,
+          style: TextStyle(fontWeight: FontWeight.w700, fontSize: 20.sp),
+        ),
+        centerTitle: true,
+        leading: IconButton(
+          icon: SvgPicture.asset(AppIcons.arrowBack),
+          onPressed: () => Navigator.pop(context),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SvgPicture.asset(
+                AppIcons.personSupport,
+              ),
+            ),
+          ),
+        ],
+      ),
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 25.sp, vertical: 50.sp),
@@ -74,157 +87,123 @@ class _VerifyOTPScreenState extends ConsumerState<VerifyOTPScreen>
             child: Form(
               key: _formKey,
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  VerticalMargin(20),
-                  Text(AppTexts.verifyOTP,
+                  SvgPicture.asset(AppImages.mailbox),
+                  VerticalMargin(30),
+                  Text(AppTexts.enterCode,
                       style: Config.h2(context).copyWith(
                         fontSize: 22.sp,
                       )),
                   VerticalMargin(5),
-                  // 📝 Note: The code below is the same as the one in the previous snippet.
-                  Text(AppTexts.verifyOTPDescription("email"),
-                      style: Config.b3(context).copyWith(
-                        color: AppColors.grey100,
-                      )),
-                  VerticalMargin(50),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20.sp),
-                    child: Pinput(
-                      defaultPinTheme: defaultPinTheme,
-                      listenForMultipleSmsOnAndroid: true,
-                      androidSmsAutofillMethod:
-                          AndroidSmsAutofillMethod.smsRetrieverApi,
-                      length: 6,
-                      pinputAutovalidateMode: PinputAutovalidateMode.onSubmit,
-                      hapticFeedbackType: HapticFeedbackType.vibrate,
-                      keyboardType: TextInputType.number,
-                      controller: _otpController,
-                      pinAnimationType: PinAnimationType.rotation,
-                      onChanged: (newOtp) {
-                        setState(() {
-                          otp.value = newOtp;
-                        });
-                        if (otp.value.isNotEmpty && otp.value.length == 6) {
-                          setState(() {
-                            isEnabled.value = true;
-                          });
-                        } else {
-                          setState(() {
-                            isEnabled.value = false;
-                          });
-                        }
-                      },
-                      errorPinTheme: defaultPinTheme.copyBorderWith(
-                        border: Border.all(color: Colors.redAccent),
-                      ),
+                  Text(widget.email, style: TextStyle(color: Color(0xFFA1A1A1), fontSize: 16.sp)),
+                  VerticalMargin(20),
+                  TextButton(
+                    onPressed: () {
+                      context.goNamed(RouteConstants.register, queryParameters: {"username": widget.username, "email": widget.email});
+                    },
+                    child: Text(
+                      AppTexts.changeEmail,
+                      style: TextStyle(fontSize: 14.sp, fontWeight: FontWeight.w500, decoration: TextDecoration.underline, color: AppColors.primaryColor),
                     ),
+                  ),
+                  VerticalMargin(25),
+                  BlocConsumer<VerifyOtpBloc, VerifyOtpState>(
+                    listener: _listenerToVerification,
+                    builder: (context, state) {
+                      return Pinput(
+                        defaultPinTheme: defaultPinTheme,
+                        focusedPinTheme: defaultPinTheme.copyDecorationWith(
+                          border: Border.all(color: AppColors.primaryColor),
+                        ),
+                        listenForMultipleSmsOnAndroid: true,
+                        androidSmsAutofillMethod: AndroidSmsAutofillMethod.smsRetrieverApi,
+                        length: 6,
+                        pinputAutovalidateMode: PinputAutovalidateMode.onSubmit,
+                        hapticFeedbackType: HapticFeedbackType.vibrate,
+                        keyboardType: TextInputType.number,
+                        controller: _otpController,
+                        pinAnimationType: PinAnimationType.fade,
+                        onChanged: (newOtp) {
+                          setState(() {
+                            otp.value = newOtp;
+                          });
+                          _verifyOtp(newOtp);
+                        },
+                        errorPinTheme: defaultPinTheme.copyBorderWith(
+                          border: Border.all(color: Colors.redAccent),
+                        ),
+                      );
+                    },
                   ),
                   const VerticalMargin(30),
-                  // Not your Email? Change it
-                  Padding(
-                    padding: EdgeInsets.only(left: 15.sp),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Text(AppTexts.notYourEmail(widget.email),
-                            style: Config.b3(context).copyWith(
-                              fontSize: 12.sp,
-                              color: AppColors.grey100.withOpacity(0.5),
-                            )),
-                        const HorizontalMargin(2),
-                        TextButton(
-                          style: ButtonStyle(
-                            overlayColor: MaterialStateProperty.all(
-                              Colors.transparent,
-                            ),
-                            enableFeedback: false,
-                            padding: MaterialStateProperty.all<EdgeInsets>(
-                                const EdgeInsets.all(0)),
-                          ),
-                          onPressed: () {
-                            context.goNamed(RouteConstants.register,
-                                queryParameters: {
-                                  "username": widget.username,
-                                  "email": widget.email
-                                });
-                          },
-                          child: Text(
-                            "Edit Email",
-                            style: Config.b3(context).copyWith(
-                              color: AppColors.secondaryColor,
-                              fontSize: 12.sp,
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  const VerticalMargin(5),
                   BlocConsumer<SignUpBloc, SignUpState>(
                       listener: _listener,
                       builder: (context, state) {
-                        return Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
                           children: [
-                            Padding(
-                              padding: EdgeInsets.only(left: 15.sp),
-                              child: Countdown(
-                                seconds: 60,
-                                controller: _controller,
-                                onFinished: () {
-                                  canResend = true;
-                                },
-                                build: (BuildContext context, double time) {
-                                  canResend = time.toInt() == 0;
-                                  return GestureDetector(
-                                    onTap: canResend
-                                        // ignore: unnecessary_lambdas
-                                        ? () async {
-                                            context
-                                                .read<SignUpBloc>()
-                                                .add(ResendOtp(
-                                                  username: widget.username,
-                                                ));
-                                          }
-                                        : null,
-                                    child: bottomTexts(
-                                      context: context,
-                                      buttonText: canResend
-                                          ? 'Resend code'
-                                          : 'Resend OTP '
-                                              '0 : ${time.toInt().toString()} seconds',
-                                      text: 'Didn’t get the code?',
-                                      textColor: canResend
-                                          ? AppColors.secondaryColor
-                                          : AppColors.grey100,
-                                    ),
-                                  );
-                                },
-                              ),
+                            Countdown(
+                              seconds: 180,
+                              controller: _controller,
+                              onFinished: () {
+                                canResend = true;
+                              },
+                              build: (BuildContext context, double time) {
+                                canResend = time.toInt() == 0;
+                                return RichText(
+                                  text: TextSpan(
+                                      text: '${AppTexts.codeExpiresIn} ',
+                                      style: TextStyle(
+                                        color: AppColors.primaryColor,
+                                      ),
+                                      children: [
+                                        TextSpan(
+                                          text: '${(time ~/ 60).toString().padLeft(2, '0')} : ${(time % 60).toInt().toString().padLeft(2, '0')}',
+                                          style: TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.w700,
+                                          ),
+                                        ),
+                                      ]),
+                                );
+                              },
                             ),
                           ],
                         );
                       }),
-                  const VerticalMargin(30),
-                  BlocConsumer<VerifyOtpBloc, VerifyOtpState>(
-                    listener: _listenerToVerification,
-                    builder: (context, state) {
-                      return AppButton(
-                        onTap: () {
-                          context.read<VerifyOtpBloc>().add(VerifyOtpEq(
-                                otp: _otpController.text,
-                                username: widget.username,
-                              ));
-                          // context.goNamed(RouteConstants.confirmPassword);
-                        },
-                        isActive: _otpController.text.length == 6,
-                        textSize: 12.sp,
-                        text: AppTexts.verify,
-                        textColor: AppColors.white100,
-                        color: AppColors.primaryColor,
-                      );
-                    },
+                  const VerticalMargin(20),
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 0, horizontal: 15.sp),
+                    decoration: BoxDecoration(
+                      color: Color(0xFFF2F4F7),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            const Icon(Icons.info, color: Colors.black),
+                            const SizedBox(width: 8),
+                            Text(AppTexts.didntGetCode),
+                          ],
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            () async {
+                              context.read<SignUpBloc>().add(ResendOtp(
+                                    username: widget.username,
+                                  ));
+                            };
+                          },
+                          child: Text(
+                            AppTexts.resendCode,
+                            style: TextStyle(fontWeight: FontWeight.w700, color: AppColors.primaryColor),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
@@ -233,6 +212,17 @@ class _VerifyOTPScreenState extends ConsumerState<VerifyOTPScreen>
         ),
       ),
     );
+  }
+
+  void _verifyOtp(String otpValue) {
+    if (otpValue.length == 6) {
+      context.read<VerifyOtpBloc>().add(
+            VerifyOtpEq(
+              otp: otpValue,
+              username: widget.username,
+            ),
+          );
+    }
   }
 
   void _listenerToVerification(BuildContext context, VerifyOtpState state) {
@@ -252,8 +242,7 @@ class _VerifyOTPScreenState extends ConsumerState<VerifyOTPScreen>
     }, otpVerificationSuccessful: (success) {
       _overlayEntry?.remove();
 
-      context.goNamed(RouteConstants.confirmPassword,
-          queryParameters: {"username": widget.username});
+      context.goNamed(RouteConstants.confirmPassword, queryParameters: {"username": widget.username});
     });
   }
 
@@ -329,19 +318,12 @@ class _VerifyOTPScreenState extends ConsumerState<VerifyOTPScreen>
   );
 
   final defaultPinTheme = PinTheme(
-      width: 56,
-      height: 56,
-      textStyle: GoogleFonts.poppins(
-        fontSize: 22,
-        color: const Color.fromRGBO(30, 60, 87, 1),
-      ),
-      decoration: BoxDecoration(
-        // Add bottom border for every pin
-        border: Border(
-          bottom: BorderSide(
-            width: 1,
-            color: const Color.fromRGBO(30, 60, 87, 1),
-          ),
-        ),
-      ));
+    width: 60,
+    height: 56,
+    textStyle: GoogleFonts.poppins(
+      fontSize: 22,
+      color: const Color.fromRGBO(30, 60, 87, 1),
+    ),
+    decoration: BoxDecoration(borderRadius: BorderRadius.circular(8), border: Border.all(width: 2, color: const Color(0xFFE2E8F0))),
+  );
 }
