2c219e8c65f5f738285f64d045ba22725c9e34fe
yunusabdul <yunusabdulmajidyunus38@gmail.com>
2025-01-27 10:19:57 +0100

    Sign Up Recovery Phrase Input

--- lib/core/global/constants/app_texts.dart
+++ lib/core/global/constants/app_texts.dart
@@ -1,3 +1,4 @@
+Path-> lib/core/global/constants/app_texts.dart
 class AppTexts {
   //! AUTHENTICATION
   static String createAccount = "Create an account";
@@ -75,65 +76,6 @@ class AppTexts {
   static String pasteOrTypePrivateKey = "Please type or paste your private key";
   static String typePrivateKey= "Paste or type private key";
   static String importMyAccount = "Import my account";
-
-  //! DASHBOARD
-  static String searchCampaign = "Search Campaign";
-  static String balance = "Balance";
-  static String topUp = "Top Up";
-  static String discoverCampaign = "Discover Campaign";
-  static String latestFundRaiser = "Latest Fundraising";
-  static String education = "Education";
-  static String placeHolderName = "Joe Doe";
-  static String donation = "Donation";
-  static String createDonation = "Create Donation";
-  static String copied = "Copied to clipboard";
-
-  //! DONATION
-  static String donationDesc = "Enter the amount you want to donate";
-  static String aboutDonation = "About Donations";
-  static String organizedBy = "Organized by";
-  static String viewDonations = "View Donations";
-  static String donate = "Donate";
-  static String viewDonors = "View Donors";
-  static String loremIpsum =
-      "This June, a mix of parents and staff are raising money in aid of Hawkedon Primary School Association and every donation will help, by completing the National 3 Peaks Challenge within 24 hours. Our brave challengers are; Rebeca Lovato, Ben Murphy, Dan Ronan, Sam Midwinter, Eddy Edwards, John O'Brien, Saskia Palmer, Oli Perry, Siva Singaravelu and Greg Jackson with our amazing volunteer driver Rachel Spencer.";
-  static String share = "Share";
-  static String shareDesc = "Share this campaign with your friends";
-  static String amount2Donate = "Amount to Donate";
-  static String proceed = "Proceed";
-  static String availableBalance = "Available Balance";
-  static String confirmDonation = "Confirm Donation Details";
-  static String success = "Successful!!!";
-  static String successDesc = "Your donation was successful";
-  static String continueText = "Go to dashboard";
-  static String walletAddr = "Wallet Address";
-  static String copyAddr = "Copy address";
-  static String campaignName = "Campaign Title";
-  static String campaignDesc = "Campaign Description";
-  static String campaignGoal = "Campaign Goal";
-  static String selectCategory = "Select Category";
-  static String deadline = "Select Deadline";
-  static String emptyMyCampaigns = "No Campaigns Yet";
-  static String emptyMyCampaignsDescription = "Create Donation";
-
-  //! NAVIGATION
-  static String home = "Home";
-  static String navDonation = "Donations";
-  static String navCampaign = "Campaigns";
-  static String navProfile = "Profile";
-
-  //! My Campaign
-  static String myCampaign = "My Campaign";
-  static String addCampaign = "Add Campaign";
-
-  //! Profile
-  static String profile = "Profile";
-  static String editProfile = "Edit Profile";
-  static String continueTo = "continue";
-  static String changePassword = "Change Password";
-  static String oldPassword = "Old Password";
-  static String newPassword = "New Password";
-  static String security = "Security";
-  static String securityDesc = "Enter Password to Access Private Key";
-  static String getPrivateKey = "Get Private Key";
-}
+  static String recoveryPhrase = "Enter your recovery phrase";
+  static String done = "Done";
+}  
